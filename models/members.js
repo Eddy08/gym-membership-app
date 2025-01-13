e144e0c7cfed1e03a598b3af2c1530b872b7ada4
Harsh <40041498+Eddy08@users.noreply.github.com>
2025-01-13 17:16:23 +0530

    Revert "Gym Membership App"
    This reverts commit 46cb21eb11c94842fe6fc285a810e68f4f768f3e.

--- models/members.js
+++
@@ -1,9 +0,0 @@
-class Member {
-  constructor(name, email, startDate) {
-    this.name = name;
-    this.email = email;
-    this.startDate = startDate;
-    this.active = true;
-  }
-}
-module.exports = Member;
