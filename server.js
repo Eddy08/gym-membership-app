3eafb800718f96b410d598156cbafa3632ef6225
Harsh <40041498+Eddy08@users.noreply.github.com>
2025-01-13 17:18:29 +0530

    App (#5)
    * Gym Membership App
    
    * removed node_modules
    
    * gym app

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const membershipRoutes = require("./routes/membership");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use("/api", membershipRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
