00938fb356ac44d9bcf801dfd6c19397375cfb62
eddy08 <bhardwajharsh08@gmail.com>
2025-01-13 16:38:20 +0530

    Gym Membership App

---
+++ models/members.js
@@ -0,0 +1,9 @@
+class Member {
+  constructor(name, email, startDate) {
+    this.name = name;
+    this.email = email;
+    this.startDate = startDate;
+    this.active = true;
+  }
+}
+module.exports = Member;
